072f1f8680af67084cf80264c36301d95bce0951
GusevADresume <resumeGusevAD@gmail.com>
2022-12-06 12:46:41 +0300

    conf_files

---
+++ js/app.js
@@ -0,0 +1,34 @@
+import Validate  from "./validate.js";
+
+class Card {
+
+    constructor(){
+        this.submit = document.querySelector('.btn');
+        this.validate = new Validate();
+        this.inputsCheck();
+
+    }
+
+    inputsCheck(){
+      let input = document.querySelector('.card_type')
+        this.submit.addEventListener('click', (e)=>{
+            if (this.validate.validate(document.querySelector('.card_type').value)){
+              let element = document.getElementsByClassName(String(this.validate.definPaymentSystem(document.querySelector('.card_type').value)));
+              element[0].classList.remove('fog')
+              input.style.background = 'green';
+            } else {
+              input.style.background = 'red';
+            }
+        })
+        
+    }
+
+    
+  }
+
+
+
+
+
+
+let crd = new Card();
